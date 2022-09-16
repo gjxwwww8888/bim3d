1baea14738904cb99b1766502ec45cf0fbb02d9b
smy <1093697597@qq.com>
2022-09-16 09:04:58 +0800

    求两条直线的交点

--- src/editor/framework/math/PolygonUtils.ts
+++ src/editor/framework/math/PolygonUtils.ts
@@ -97,4 +97,42 @@ export default class PolygonUtils {
         return intersects;
     }
 
+    /**
+     * 求直线的交点
+     * @param a line start
+     * @param b line end
+     * @param c line start
+     * @param d line end
+     * @returns 
+     */
+    static segmentsIntr(a:Vector2, b:Vector2, c:Vector2, d:Vector2):Vector3{  
+  
+        // 三角形abc 面积的2倍  
+        var area_abc = (a.x - c.x) * (b.y - c.y) - (a.y - c.y) * (b.x - c.x);  
+      
+        // 三角形abd 面积的2倍  
+        var area_abd = (a.x - d.x) * (b.y - d.y) - (a.y - d.y) * (b.x - d.x);   
+      
+        // 面积符号相同则两点在线段同侧,不相交 (对点在线段上的情况,本例当作不相交处理);  
+        // if ( area_abc*area_abd>=0 ) {  
+        //     return false;  
+        // }  
+      
+        // 三角形cda 面积的2倍  
+        var area_cda = (c.x - a.x) * (d.y - a.y) - (c.y - a.y) * (d.x - a.x);  
+        // 三角形cdb 面积的2倍  
+        // 注意: 这里有一个小优化.不需要再用公式计算面积,而是通过已知的三个面积加减得出.  
+        // var area_cdb = area_cda + area_abc - area_abd ;  
+        // if (  area_cda * area_cdb >= 0 ) {  
+        //     return false;  
+        // }  
+      
+        //计算交点坐标  
+        var t = area_cda / ( area_abd- area_abc );  
+        var dx= t*(b.x - a.x),  
+            dy= t*(b.y - a.y);  
+        return new Vector3(a.x + dx , 0, a.y + dy);  
+      
+    }  
+
 }
