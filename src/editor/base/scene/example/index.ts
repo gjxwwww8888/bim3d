c566685b056b2c88b567858618790f3fc5b51cfa
smy <1093697597@qq.com>
2022-09-23 09:21:02 +0800

    顶点颜色

--- src/editor/base/scene/example/index.ts
+++ src/editor/base/scene/example/index.ts
@@ -1,4 +1,4 @@
-import { BoxGeometry, DirectionalLight, DoubleSide, HemisphereLight, Mesh, MeshPhongMaterial, PerspectiveCamera, Scene, sRGBEncoding, Vector3, WebGLRenderer } from "three";
+import { BoxGeometry, BufferAttribute, DirectionalLight, DoubleSide, HemisphereLight, Mesh, MeshPhongMaterial, PerspectiveCamera, Scene, sRGBEncoding, Vector3, WebGLRenderer } from "three";
 
 export default class ExampleScene {
 
@@ -9,10 +9,10 @@ export default class ExampleScene {
     /** 渲染 */
     private _render: WebGLRenderer;
 
-    private _viewWidth:number;
-    private _viewHeight:number;
+    private _viewWidth: number;
+    private _viewHeight: number;
 
-    private _box:Mesh;
+    private _box: Mesh;
 
     get scene(): Scene {
         return this._scene;
@@ -40,28 +40,36 @@ export default class ExampleScene {
     private createScene(): void {
         this._scene = new Scene();
         // this._scene.background = new Color(0xfff)
-     
+
     }
 
-    private addBox():void {
-        let box = new BoxGeometry(5,5,5);
+    private addBox(): void {
+        let box = new BoxGeometry(5, 5, 5);
         let mat = new MeshPhongMaterial({
-            color: 0xff0000,
-            side:DoubleSide,
-        })
+            vertexColors: true
+        });
+        const colors = (box.getAttribute('position').array as any).slice();
+        for (let i = 0, l = colors.length; i < l; i++) {
+
+            if (colors[i] > 0) colors[i] = 0.5;
+            else colors[i] = 0;
+
+        }
+
+        box.setAttribute('color', new BufferAttribute(colors, 3, false));
 
         this._box = new Mesh(box, mat);
-        this._scene.add( this._box);
+        this._scene.add(this._box);
     }
 
-    private createPCamera():void {
-        this._pcamera = new PerspectiveCamera(60, this._viewWidth  / this._viewHeight, 0.01, 1000);
+    private createPCamera(): void {
+        this._pcamera = new PerspectiveCamera(60, this._viewWidth / this._viewHeight, 0.01, 1000);
         this._pcamera.position.set(10, 10, 10);
         this._pcamera.lookAt(new Vector3(0, 0, 0));
     }
 
-     /** 添加光 */
-     private createLight(): void {
+    /** 添加光 */
+    private createLight(): void {
         // 环境光，全局光照
         const hemiLight = new HemisphereLight(0xffffff, 0x33333, 0.6);
         hemiLight.position.set(0, 200, 0);
@@ -88,8 +96,8 @@ export default class ExampleScene {
     }
 
 
-    update():void {
-        if(this._box){
+    update(): void {
+        if (this._box) {
             this._box.rotation.x += 0.01;
             this._box.rotation.y += 0.01;
             this._box.rotation.z += 0.01;
