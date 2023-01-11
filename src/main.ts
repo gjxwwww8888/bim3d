5edf1f195ccd5c47f55e39cdb937370ad5d32c5a
smy <1093697597@qq.com>
2023-01-11 09:21:30 +0800

    reset 3d

--- src/main.ts
+++ src/main.ts
@@ -1,12 +1,12 @@
 
-// import BIM from './editor/BIM';
+import BIM from './editor/BIM';
 import ReactDom from './views/App';
 
 export class main {
 
   constructor() {
     // 3d
-    // new BIM().startUp();
+    new BIM().startUp();
     // UI
     new ReactDom();
 
