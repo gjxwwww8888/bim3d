6885c291d662bb07bd52142cd0be74791a2e86a7
smy <1093697597@qq.com>
2023-07-04 18:13:17 +0800

    render timer

---
+++ src/editor/base/mgr/RenderTimer.ts
@@ -0,0 +1,14 @@
+/**
+ * @description 分时渲染
+ * @author songmy
+ */
+export default class RenderTimer {
+
+    /**
+     * 更新渲染
+     * @param limit 每次提交的渲染个数
+     */
+    update(limit:number = 10000){
+        
+    }
+}
